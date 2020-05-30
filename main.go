10f8f99fee1dde70af9cae9907c992ec056af249
Mourey jerome <jerome@everycheck.fr>
2020-05-30 22:31:43 +0200

    main can parse file

--- main.go
+++ main.go
@@ -1,31 +1,51 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
-	"os"
 	"pml/lexer"
+	"pml/parser"
 	"pml/token"
 )
 
 func main() {
 
-	if len(os.Args) < 2 {
-		fmt.Println("You need to provide an input file")
+	mode := flag.String("mode", "lexer", "mode : lexer|parser")
+	filename := flag.String("filename", "", "entry filename")
+
+	flag.Parse()
+
+	if len(*filename) == 0 {
+		flag.PrintDefaults()
 		return
 	}
 
-	file, err := ioutil.ReadFile(os.Args[1])
+	file, err := ioutil.ReadFile(*filename)
 	if err != nil {
-		fmt.Println("Cannot find file " + os.Args[1])
+		fmt.Println("Cannot find file " + *filename)
 		return
 	}
 
 	l := lexer.New(string(file))
 
-	tok := l.GetNextToken()
-	for tok.Type != token.EOF {
-		fmt.Println(tok)
-		tok = l.GetNextToken()
+	switch *mode {
+	case "lexer":
+		tok := l.GetNextToken()
+		for tok.Type != token.EOF {
+			fmt.Println(tok)
+			tok = l.GetNextToken()
+		}
+	case "parser":
+		p := parser.New(l)
+		item, err := p.Parse()
+		if err != nil {
+			fmt.Printf("failed : %v", err)
+			return
+		}
+		fmt.Println(item)
+	default:
+		fmt.Printf("Mode not handle : " + *mode)
+		flag.PrintDefaults()
 	}
 }
