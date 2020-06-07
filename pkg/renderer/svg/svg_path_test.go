74c3d24286a292d59fc15b423f384cb735b68ac5
Mourey jerome <jerome@everycheck.fr>
2020-06-07 11:21:39 +0200

    fix test in svg parser

--- pkg/renderer/svg/svg_path_test.go
+++ pkg/renderer/svg/svg_path_test.go
@@ -15,13 +15,13 @@ func TestPathParsing(t *testing.T) {
 		expected []svgpath.Command
 	}{
 		{
-			path:     "M148,148C148,88.448 196.449,40 256,40C315.551,40 364,88.448 364,148L10,12.33 21,23Z",
+			path:     "M148,148C148,88.448 196.449,40 256,40C315.551,40 364,88.448 364,148L10,12.33Z",
 			tranform: matrix.Identity(),
 			expected: []svgpath.Command{
 				{'M', []svgpath.Point{{148, 148}}},
 				{'C', []svgpath.Point{{148, 88.448}, {196.449, 40}, {256, 40}}},
 				{'C', []svgpath.Point{{315.551, 40}, {364, 88.448}, {364, 148}}},
-				{'L', []svgpath.Point{{10, 12.33}, {21, 23}}},
+				{'L', []svgpath.Point{{10, 12.33}}},
 				{'Z', []svgpath.Point{}},
 			},
 		},
@@ -46,7 +46,7 @@ func TestPathParsing(t *testing.T) {
 		result, err := svgPath(pathElement, tt.tranform)
 
 		if err != nil {
-			t.Fatalf("[%d] should not failed but got %v", i, err)
+			t.Fatalf("[%s] should not failed but got %v", tt.path, err)
 		}
 
 		if len(result.commands) != len(tt.expected) {
--- pkg/renderer/svg/svgpath/main.go
+++ pkg/renderer/svg/svgpath/main.go
@@ -1,16 +1,21 @@
 package svgpath
 
+import (
+	"errors"
+)
+
 func Parse(path string) ([]Command, error) {
 	l := newLexer(path)
 	parser := newParser(l)
 	commands, err := parser.parse()
+
 	if err != nil {
 		return nil, err
 	}
 
 	for _, cmd := range commands {
 		if validateCommand(cmd) == false {
-			return nil, err
+			return nil, errors.New("InvalideCommandFound : " + string(cmd.Kind))
 		}
 	}
 	return commands, nil
