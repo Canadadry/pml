1478d9f335a55e12745126215e045014093c50f6
Mourey jerome <jerome@everycheck.fr>
2020-06-07 22:23:22 +0200

    add parsing basic style

--- pkg/renderer/svg/svg_style.go
+++ pkg/renderer/svg/svg_style.go
@@ -1,91 +1,83 @@
 package svg
 
 import (
+	"errors"
 	"image/color"
 	"pml/pkg/renderer/svg/svgparser"
+	"strconv"
+	"strings"
 )
 
 func parseStyleAttribute(element *svgparser.Element) Style {
 	s := Style{
-		Fill:        true,
-		FillColor:   color.RGBA{255, 0, 0, 0},
-		BorderSize:  0.1,
-		BorderColor: color.RGBA{0, 255, 0, 0},
+		Fill:        false,
+		FillColor:   color.RGBA{0, 0, 0, 0},
+		BorderSize:  0,
+		BorderColor: color.RGBA{0, 0, 0, 0},
+	}
+
+	style, ok := element.Attributes["style"]
+	if !ok {
+		return s
+	}
+
+	part := strings.Split(style, ";")
+
+	for _, p := range part {
+		arg := strings.Split(p, ":")
+		if len(arg) != 2 {
+			continue
+		}
+		switch arg[0] {
+		case "fill":
+			c, err := parseColorParam(arg[1])
+			if err != nil {
+				continue
+			}
+			s.FillColor = c
+			s.Fill = true
+		case "stroke":
+			c, err := parseColorParam(arg[1])
+			if err != nil {
+				continue
+			}
+			s.BorderColor = c
+			s.BorderSize = 0.1
+		case "stroke-width":
+			s.BorderSize = 0.1
+		}
 	}
 
 	return s
 }
 
-// parseStyleFromAttributes(svg:string)
-// {
-// 	let fill:boolean = false;
-// 	let stroke:boolean = false;
+func parseColorParam(attribute string) (color.RGBA, error) {
 
-// 	let styles = svg.split(';');
-// 	for(let style of styles)
-// 	{
-// 		let arg = style.split(':');
-// 		switch (arg[0])
-// 		{
-// 			case "fill":
-// 			{
-// 				fill =  true;
-// 				if(arg[1].substring(0,4) == 'rgb(')
-// 				{
-// 					this.style.fillColor = arg[1].substring(4,arg[1].length-1).split(',').map(c=>parseFloat(c));
-// 				}
-// 				else
-// 				{
-// 					let dict = this.getColorDictionnary();
-// 					if(dict.hasOwnProperty(arg[1]))
-// 					{
-// 						this.style.fillColor = dict[arg[1]];
-// 					}
-// 				}
-// 				break;
-// 			}
-// 			case "stroke":
-// 			{
-// 				stroke =  true;
-// 				if(arg[1].substring(0,4) == 'rgb(')
-// 				{
-// 					this.style.stokeColor = arg[1].substring(4,arg[1].length-1).split(',').map(c=>parseFloat(c));
-// 				}
-// 				else
-// 				{
-// 					let dict = this.getColorDictionnary();
-// 					if(dict.hasOwnProperty(arg[1]))
-// 					{
-// 						this.style.fillColor = dict[arg[1]];
-// 					}
-// 				}
-// 				break;
-// 			}
-// 			case "stroke-width":
-// 			{
-// 				stroke =  true;
-// 				if(arg[1].substring(arg[1].length-2,arg[1].length) == 'px')
-// 				{
-// 					let width = parseFloat(arg[1].substring(0,arg[1].length-2));
-// 					let newWidth = this.worldToLocal.multiplyPoint(width,0,0);
-// 					this.style.strokeWidth = newWidth.x;
-// 				}
-// 				break;
-// 			}
+	if len(attribute) <= 4 || attribute[:4] != "rgb(" {
+		rgb, ok := colorDict[attribute]
+		if !ok {
+			return color.RGBA{}, errors.New("ColorNotFoundInDictionnary")
+		}
+		return rgb, nil
+	}
 
-// 		}
-// 	}
+	colorAttr := attribute[4 : len(attribute)-1]
+	colorPart := strings.Split(colorAttr, ",")
+	if len(colorPart) != 3 {
+		return color.RGBA{}, errors.New("NotEnoughPartToParseRGB")
+	}
+	r, err := strconv.ParseUint(colorPart[0], 10, 8)
+	if err != nil {
+		return color.RGBA{}, errors.New("ParseUintFailed")
+	}
+	g, err := strconv.ParseUint(colorPart[1], 10, 8)
+	if err != nil {
+		return color.RGBA{}, errors.New("ParseUintFailed")
+	}
+	b, err := strconv.ParseUint(colorPart[2], 10, 8)
+	if err != nil {
+		return color.RGBA{}, errors.New("ParseUintFailed")
+	}
 
-// 	if( fill && stroke)
-// 	{
-// 		this.style.mode = 'FD';
-// 	}
-// 	else if( fill )
-// 	{
-// 		this.style.mode = 'F';
-// 	}
-// 	else
-// 	{
-// 		this.style.mode = 'S';
-// 	}
-// }
+	return color.RGBA{uint8(r), uint8(g), uint8(b), 0}, nil
+}
