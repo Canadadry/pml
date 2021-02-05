5db5655177cf9e7f3364322168ec13cd4a74cb89
Mourey jerome <jerome@everycheck.fr>
2021-02-05 11:42:59 +0100

    fix testing

--- compiler/renderer/render_box_test.go
+++ compiler/renderer/render_box_test.go
@@ -12,52 +12,52 @@ func TestCut(t *testing.T) {
 	}{
 		{
 			In:    renderBox{0, 0, 100, 100},
-			Frame: Frame{10, 10, 80, 80, Free, Free},
+			Frame: Frame{10, 10, 80, 80, Relative, Relative},
 			Exp:   renderBox{10, 10, 80, 80},
 		},
 		{
 			In:    renderBox{10, 10, 100, 100},
-			Frame: Frame{15, 15, 80, 80, Free, Free},
+			Frame: Frame{15, 15, 80, 80, Relative, Relative},
 			Exp:   renderBox{25, 25, 80, 80},
 		},
 		{
 			In:    renderBox{10, 10, 100, 100},
-			Frame: Frame{10, 10, 80, 80, Left, Free},
+			Frame: Frame{10, 10, 80, 80, Left, Relative},
 			Exp:   renderBox{10, 20, 80, 80},
 		},
 		{
 			In:    renderBox{10, 10, 100, 100},
-			Frame: Frame{10, 10, 80, 80, Center, Free},
+			Frame: Frame{10, 10, 80, 80, Center, Relative},
 			Exp:   renderBox{20, 20, 80, 80},
 		},
 		{
 			In:    renderBox{10, 10, 100, 100},
-			Frame: Frame{10, 10, 80, 80, Right, Free},
+			Frame: Frame{10, 10, 80, 80, Right, Relative},
 			Exp:   renderBox{30, 20, 80, 80},
 		},
 		{
 			In:    renderBox{10, 10, 100, 100},
-			Frame: Frame{10, 10, 10, 80, Fill, Free},
+			Frame: Frame{10, 10, 10, 80, Fill, Relative},
 			Exp:   renderBox{10, 20, 100, 80},
 		},
 		{
 			In:    renderBox{10, 10, 100, 100},
-			Frame: Frame{10, 10, 80, 80, Free, Top},
+			Frame: Frame{10, 10, 80, 80, Relative, Top},
 			Exp:   renderBox{20, 10, 80, 80},
 		},
 		{
 			In:    renderBox{10, 10, 100, 100},
-			Frame: Frame{10, 10, 80, 80, Free, Center},
+			Frame: Frame{10, 10, 80, 80, Relative, Center},
 			Exp:   renderBox{20, 20, 80, 80},
 		},
 		{
 			In:    renderBox{10, 10, 100, 100},
-			Frame: Frame{10, 10, 80, 80, Free, Bottom},
+			Frame: Frame{10, 10, 80, 80, Relative, Bottom},
 			Exp:   renderBox{20, 30, 80, 80},
 		},
 		{
 			In:    renderBox{10, 10, 100, 100},
-			Frame: Frame{10, 10, 80, 20, Free, Fill},
+			Frame: Frame{10, 10, 80, 20, Relative, Fill},
 			Exp:   renderBox{20, 10, 80, 100},
 		},
 	}
